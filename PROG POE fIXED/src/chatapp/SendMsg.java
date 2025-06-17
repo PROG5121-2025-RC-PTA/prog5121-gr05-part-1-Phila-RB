44bc162f09f6a35a1527c8ffb9bebe3c1fbb1410
ST10486606 <ST10486606@rcconnect.edu.za>
2025-06-18 00:00:57 +0200

    Delete PROG POE fIXED directory

--- PROG POE fIXED/src/chatapp/SendMsg.java
+++
@@ -1,5 +0,0 @@
-package chatapp;
-
-public interface SendMsg {
-	public void msgToSend(String msg);
-}
--- PROG POE fIXED/src/chatapp/SendTheMessageAlready.java
+++
@@ -1,7 +0,0 @@
-package chatapp;
-
-public interface SendTheMessageAlready {
-	public void sending();
-	public void disregarding();
-	public void storing();
-}
